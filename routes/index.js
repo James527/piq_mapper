13c2bd871d8d34ac9007389c3d7f66ab9db92a01
James Rutledge <jarutledge2@gmail.com>
2015-06-29 17:55:53 -0500

    Took out the title

--- routes/index.js
+++ routes/index.js
@@ -5,27 +5,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PiqMapper', piq: {'color':'#ff0000'} });
+  res.render('index', { piq: {'color':'#ff0000'} });
 });
 
 /* GET register form. */
 router.get('/register', function(req, res, next) {
-  res.render('register_form', { title: 'PiqMapper' });
+  res.render('register_form');
 });
 
 /* GET login form. */
 router.get('/login', function(req, res, next) {
-  res.render('login_form', { title: 'PiqMapper' });
+  res.render('login_form');
 });
 
 /* GET piq form. */
 router.get('/piq_form', function(req, res, next) {
-	res.render('piq_form', { title: 'PiqMapper' });
+	res.render('piq_form');
 });
 
 /* GET piqs page. */
 router.get('/piqs', function(req, res, next) {
-	res.render('piqs', { title: 'PiqMapper', piqs: {'color':'#ff0000'} });
+	res.render('piqs', { piqs: {'color':'#ff0000'} });
 });
 
 /* POST registration. */
@@ -38,19 +38,19 @@ router.post('/register', function(req, res, next) {
 	// });
 	// var salt = bcrypt.genSaltSync(10);
 	// var hash = bcrypt.hashSync("B4c0/\/", salt);
-  res.render('index', { title: 'PiqMapper', piq: {'color':'#ff0000'} });
+  res.render('index', { piq: {'color':'#ff0000'} });
 });
 
 /* POST login. */
 router.post('/login', function(req, res, next) {
 	// TODO:
-  res.render('index', { title: 'PiqMapper', piq: {'color':'#ff0000'} });
+  res.render('index', { piq: {'color':'#ff0000'} });
 });
 
 /* POST piq. */
 router.post('/piq_form', function(req, res, next) {
 	// TODO:
-  res.render('index', { title: 'PiqMapper', piq: {'color':'#ff0000'} });
+  res.render('index', { piq: {'color':'#ff0000'} });
 });
 
 // app.get('/piqs', function(req, res) {
