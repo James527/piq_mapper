a2392762b353c9a0809ac9b4007f605af112616b
James Rutledge <jarutledge2@gmail.com>
2015-07-06 23:06:51 -0500

    ...

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
