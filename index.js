8bbe6eff9412e9f1d67105a962983a657c6662ad
James Rutledge <jarutledge2@gmail.com>
2015-07-07 22:13:30 -0500

    Body-parser module

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 // var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
